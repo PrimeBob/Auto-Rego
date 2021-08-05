5c1c7ff4993a006e3b7fd58af9b384ecfd66cb3e
jason <73451346+PrimeBob@users.noreply.github.com>
2021-08-05 16:23:46 +1000

    Update auto_rego.py

--- auto_rego.py
+++ auto_rego.py
@@ -94,7 +94,7 @@ countrydictionary={
 # In[154]:
 
 
-web = webdriver.Chrome(chrome_options=options, executable_path='/Users/'+getpass.getuser()+'/Desktop/auto-rego/bin/chromedriver')
+web = webdriver.Chrome(chrome_options=options, executable_path='/Users/'+getpass.getuser()+'/Desktop/auto-rego/chromedriver')
 
 ##the below code fills in the registration page and submits it
 for i in range(len(harvestfield.email)):        
